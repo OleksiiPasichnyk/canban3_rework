df7ae288e31fe8901719fcf9826a5a51bc198304
Axel <axelpasechniki@gmail.com>
2020-09-05 00:47:46 -0400

    Updates

---
+++ precache-manifest.80da0bf5d4a6b97ea6118e8afdb0c9c5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a1f2a30c0a7e3d85f734ee6c85548001",
+    "url": "/canban3_rework/index.html"
+  },
+  {
+    "revision": "1e754e283bc969633fcd",
+    "url": "/canban3_rework/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "737251cdc68fa90e1ed3",
+    "url": "/canban3_rework/static/css/main.523f09c5.chunk.css"
+  },
+  {
+    "revision": "1e754e283bc969633fcd",
+    "url": "/canban3_rework/static/js/2.5d0225af.chunk.js"
+  },
+  {
+    "revision": "d2966845b94a3318bf32eecc7af8015d",
+    "url": "/canban3_rework/static/js/2.5d0225af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "737251cdc68fa90e1ed3",
+    "url": "/canban3_rework/static/js/main.425e07b2.chunk.js"
+  },
+  {
+    "revision": "95aa455795d7a41ed3d0",
+    "url": "/canban3_rework/static/js/runtime-main.9472af5f.js"
+  }
+]);
